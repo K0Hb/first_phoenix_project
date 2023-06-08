d136217d512f3265fb96c7014d682c141637e26d
v.a.konovalov <v.a.konovalov@fun-box.ru>
2023-06-08 10:20:37 +0300

    add validation %Word{}

--- lib/first_phoenix_project/word.ex
+++ lib/first_phoenix_project/word.ex
@@ -1,8 +1,16 @@
 defmodule FirstPhoenixProject.Word do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "words" do
     field :eng, :string
     field :rus, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:eng, :rus])
+    |> validate_required([:eng, :rus])
+    |> unique_constraint(:eng)
   end
 end
